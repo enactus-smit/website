68836009ebb0773c98fc902b8197e0eb07554bc9
abhrajitray77 <abhrajitray77@gmail.com>
2023-01-31 21:10:56 +0530

    added imgs

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import Nav from "./components/Nav";
 function App() {
   return (
-    <div className="App">
+    <div className="h-screen w-screen">
       <Nav />
     </div>
   );
