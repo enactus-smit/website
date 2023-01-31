9ac830782e29c244456f5cbe93eded613cf64e01
abhrajitray77 <abhrajitray77@gmail.com>
2023-01-31 22:36:23 +0530

    notes added

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 const Nav = () => {
   return (
-    <div className=" bg-[#747171] min-w-max text-white ">
+    <div className=" bg-[#444040] min-w-max text-white ">
       <div className="py-2 px-4 flex justify-between">
         <h1 className="text-4xl">Enactus Logo</h1>
         <ul className="flex text-center justify-between">
           <li>Our Projects</li>
+          <li>Collaboration</li>
           <li>Constution</li>
+          
           <li>Events</li>
           <li>Contact Us</li>
         </ul>
