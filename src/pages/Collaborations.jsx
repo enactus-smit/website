ce7cc5cca0583ef4898d5eb8facbc3aefc6c8c0f
Kunal Sharma <70373330+SHARK2002@users.noreply.github.com>
2023-02-04 14:03:28 +0530

    Added collaborations page

--- src/pages/Collaborations.jsx
+++ src/pages/Collaborations.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-
+import React from "react";
+import CollabCard from "../components/CollabCard";
+import hult_prize from "../imgs/hult_prize.png";
+import art from "../imgs/Art.png";
 const Collaborations = () => {
   return (
-    <div>Collaborations</div>
-  )
-}
+    <div>
+      <h1 className="text-center my-5 text-4xl">Collaborations</h1>
+      <div className="lg:flex flex-row lg:justify-evenly px-10 ">
+        <CollabCard
+          name="Hult Prize Foundation"
+          url="https://www.hultprize.org/"
+          img={hult_prize}
+        />
 
-export default Collaborations
+        <CollabCard
+          name="Art Plant Paint"
+          url="https://www.instagram.com/art.plant.paint/?igshid=ZDdkNTZiNTM%3D"
+          owner="Tanushka Chauhan"
+          img={art}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Collaborations;
