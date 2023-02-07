74812eae7ccddc33c7866a39f71d91b68dc18416
abhrajitray77 <abhrajitray77@gmail.com>
2023-02-07 22:32:09 +0530

    img collab fixed

--- src/components/CollabCard.jsx
+++ src/components/CollabCard.jsx
@@ -2,20 +2,18 @@ import React from "react";
 
 const CollabCard = (props) => {
   return (
-    <div className="p-10">
+    <div className="flex flex-col p-10">
       <a href={props.url}>
         <div
-          className="bg-[#8c929933] rounded-3xl shadow-lg h-[18rem] max-w-[300px] min-w-[300px] mx-auto p-6 hover:bg-yellow-400 duration-700"
+          className="bg-[#8c929933] rounded-3xl shadow-lg h-[18rem] w-[300px] mx-auto p-6 hover:bg-yellow-400 duration-700"
           
         >
           <img
             src={props.img}
             className="mx-auto rounded-lg max-h-28"
             alt={props.name}
-            width={100}
-            height = {100}
           />
-          <h1 className="text-center mt-6 text-2xl font-bold">{props.name}</h1>
+          <h1 className=" text-center mt-6 text-2xl font-bold">{props.name}</h1>
           <h1 className="text-lg text-center">{props.owner}</h1>
         </div>
       </a>
