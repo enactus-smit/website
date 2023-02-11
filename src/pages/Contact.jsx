4445143c8fe9e67f9b6654fc4b2f8c3e31d5d2d6
abhrajitray77 <abhrajitray77@gmail.com>
2023-02-12 00:29:21 +0530

    Contact Us page added

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import sideImg from "../imgs/Enactus_Full_Color_logo.png"
+import Footer from "../components/Footer";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="w-auto">
+      <section className="grid grid-cols-1 md:grid-cols-2 w-full h-auto md:pr-10">
+        <div className="grid h-full w-full place-content-center">
+          <img src={sideImg} alt="side_img" />
+        </div>
+        <div className="flex flex-col w-full mt-6 bg-[#ffffffbd] p-8 rounded-3xl">
+          <h1 className="flex justify-center text-[3rem] font-semibold">
+            Contact Us
+          </h1>
+          <p className="flex justify-center text-lg font-medium">
+            Feel free to send us your valuable feedback!
+          </p>
+          <form className="flex flex-col h-auto w-auto space-y-12 mt-10 px-20">
+            <input placeholder="Name *" className="input-boxes">
+            </input>
+            <input placeholder="E-mail *" className="input-boxes ">
+            </input>
+
+            <input placeholder="Subject *" className="input-boxes">
+            </input>
+            <input placeholder="Type your message here *" className="
+            pb-36 p-2 rounded-lg ring-4 ring-yellow-400 outline-4 !outline-yellow-400">
+            </input>
+            <button className="bg-yellow-400 w-[30%] p-3 mx-auto rounded-md hover:bg-yellow-500">
+              Submit
+            </button>
+          </form>
+        </div>
+      </section>
+      <div className="mx-10 mt-10">
+        <Footer />
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
