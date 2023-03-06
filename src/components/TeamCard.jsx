212997ebfadca3ce21bbd67b956618fff8641145
Kunal Sharma <70373330+SHARK2002@users.noreply.github.com>
2023-03-07 01:29:25 +0530

    completed the teams page

--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -30,17 +30,18 @@ const TeamCard = (props) => (
   </div>
 );
 
-const TeamNameCard = ({ name, members }) => {
+const TeamNameCard = ({ name, members, director }) => {
   return (
-    <div className="w-48 h-64 m-4 rounded overflow-hidden shadow-lg bg-white">
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{name}</div>
-        <ul>
-          {members.map((member) => (
-            <li className="mb-2">{member} </li>
-          ))}
-        </ul>
+    <div className="w-48 lg:w-72 h-64 m-4 overflow-hidden shadow-lg bg-white text-center rounded-xl ">
+      <div className="pt-4 pb-2 bg-yellow-400">
+        <div className="font-bold text-xl mb-2 ">{name}</div>
       </div>
+      <div className="text-lg font-semibold pt-2 ">{director}</div>
+      <ul>
+        {members.map((member) => (
+          <li className="mb-2">{member} </li>
+        ))}
+      </ul>
     </div>
   );
 };
--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -49,22 +49,56 @@ const Team = () => {
 
   const teamNames = [
     {
-      name: "Team",
-      members: ["Member", "Member", "Member"],
+      name: "Engagement",
+      director: "Gunjan Chakrabartty",
+      members: [
+        "Abhinav Anand",
+        "Abhrajit ray",
+        "Asmita Mukherjee",
+        "Nishita Dutta",
+      ],
     },
     {
-      name: "Team",
-      members: ["Member", "Member", "Member"],
+      name: "Event Management",
+      director: "Rythem Goyal",
+      members: [
+        "Anauksa Das",
+        "Arpan Ari",
+        "Devratna",
+        "Indraneel",
+        "Jatin Karki",
+        "Koustabh Das",
+        "Neelvie Chhetri",
+        "Nikita Kar",
+        "Rheah Pundi",
+      ],
     },
     {
-      name: "Team",
-      members: ["Member", "Member", "Member"],
+      name: "Media & Communications",
+      director: "Jatin Sharma",
+      members: [
+        "Gourav",
+        "Keshav Gupta",
+        "Manasvi Gupta",
+        "Nirzor Choudhary",
+        "Yeting Heruka",
+      ],
+    },
+    {
+      name: "Outreach",
+      director: "Achintya Sharma",
+      members: ["Adrija Chakrabarti", "Kusangla Lepcha", "Torsha Guha"],
+    },
+    {
+      name: "Survey",
+      director: "Suryasnato Mitra",
+      members: ["Ayusmaan Panigrahi ", "Gamin Ghaley", "Soheli Das"],
     },
   ];
   return (
     <div>
       <h1 className="flex justify-center whitespace-nowrap text-[3rem] md:text-[3.5rem] lg:text-[4.2rem] font-semibold">
-        Teams
+        Our Team
       </h1>
 
       <div className="flex flex-col">
@@ -83,7 +117,11 @@ const Team = () => {
         </div>
         <div className="flex flex-wrap justify-center">
           {teamNames.map((teamName) => (
-            <TeamNameCard name={teamName.name} members={teamName.members} />
+            <TeamNameCard
+              name={teamName.name}
+              members={teamName.members}
+              director={teamName.director}
+            />
           ))}
         </div>
       </div>
