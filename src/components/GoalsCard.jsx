b67000a34d6b06bc5a8ee7efb4fe2815dc937e61
abhrajitray77 <abhrajitray77@gmail.com>
2023-03-08 17:16:39 +0530

    Added animation to goals cards

--- src/components/GoalsCard.jsx
+++ src/components/GoalsCard.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import { motion, useScroll, Variants } from "framer-motion"
+
+
+const cardVariants = {
+  offscreen: {
+    y: 100,
+    opacity: 0
+  },
+  onscreen: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.8
+    }
+  }
+};
 
 const GoalsCard = (props) => {
   return (
     <div className="flex flex-col p-10">
       <a href={props.url}>
+      <motion.div
+      className="word-container"
+      initial="offscreen"
+      whileInView="onscreen"
+      variants={cardVariants}
+      >
         <div
           className="bg-[#ffffffd5] rounded-3xl shadow-lg h-auto w-[300px] mx-auto p-6
            hover:bg-slate-200 duration-700"
@@ -17,6 +39,7 @@ const GoalsCard = (props) => {
           <h1 className=" text-center text-gray-800 mt-6 text-xl font-bold">{props.name}</h1>
           <h1 className="text-lg text-center">{props.owner}</h1>
         </div>
+        </motion.div>
       </a>
     </div>
   );
