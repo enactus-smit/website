5c005b66ef01b957207c0ecbe0a9a9100c293248
abhrajitray77 <abhrajitray77@gmail.com>
2023-03-09 19:47:03 +0530

    contact Us fixed

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,7 +5,7 @@ import Footer from "../components/Footer";
 const Contact = () => {
   return (
     <div className="h-screen w-auto">
-      <section className="grid grid-cols-1 md:grid-cols-2 w-full md:pr-10 h-screen mb-10">
+      <section className="grid grid-cols-1 md:grid-cols-2 w-full md:pr-10 mb-10">
         <div className="grid h-full w-full place-content-center">
           <img src={sideImg} alt="side_img" />
         </div>
@@ -17,16 +17,25 @@ const Contact = () => {
             Feel free to send us your valuable feedback!
           </p>
           <form className="flex flex-col h-auto w-auto space-y-12 mt-10 md:px-10 lg:px-20">
-            <input placeholder="Name *" className="input-boxes">
-            </input>
-            <input placeholder="E-mail *" className="input-boxes ">
-            </input>
-
-            <input placeholder="Subject *" className="input-boxes">
-            </input>
-            <input placeholder="Type your message here *" className="
-            pb-36 p-2 rounded-lg ring-4 ring-yellow-400 outline-4 !outline-yellow-400">
-            </input>
+            <div className="flex flex-col space-y-2">
+              <label className="text-lg font-medium">Name: </label>
+              <input placeholder="Name *" className="input-boxes"></input>
+            </div>
+            <div className="flex flex-col space-y-2">
+              <label className="text-lg font-medium">E-mail: </label>
+              <input placeholder="E-mail *" className="input-boxes "></input>
+            </div>
+            <div className="flex flex-col space-y-2">
+              <label className="text-lg font-medium">Subject: </label>
+              <input placeholder="Subject *" className="input-boxes"></input>
+            </div>
+            <div className="flex flex-col space-y-2">
+              <label className="text-lg font-medium">Message: </label>
+              <input
+                placeholder="Type your message here *"
+                className="pb-36 p-2 rounded-lg ring-4 ring-gray-400 outline-4 !outline-gray-700"
+              ></input>
+            </div>
             <button className="bg-yellow-400 w-[30%] p-3 mx-auto rounded-md hover:bg-yellow-500">
               Submit
             </button>
